4abd8f6df57c92c5726f5348517af9b97818dda1
Sadullaev Akhrorbek <jw@jwui-MacBookPro.local>
2025-02-17 01:01:58 +0900

    Loyiha yuklandi

--- src/utils/database.config.js
+++ src/utils/database.config.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 import { DB_URL } from "./secrets.js";
 
-export const MONGODB_CONNECT  = async () =>{
+export const MONGODB_CONNECT  = async () => {
     try {
         const { connections } = await connect(DB_URL);
         console.log(`DB CONNECTED! NAME:${connections[0].name}! HOST:${connections[0].host}`);
